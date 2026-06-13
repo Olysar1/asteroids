46fe4c39d3a91ffa9e87d99a555b728b00621d5d
Olysar1 <giorgibarbakadz1@gmail.com>
2026-06-13 20:12:13 +0400

    add collision detection. resolve conflict

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import pygame
-from logger import log_state
+import sys
+from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroifs with pygme verion: {pygame.version.ver}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    asteroidfield = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    field = AsteroidField()
 
     while True:
         log_state()
@@ -14,9 +31,21 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
+        dt = clock.tick(60) / 1000
         screen.fill("black")
-        pygame.display.flip()
+        updatable.update(dt)
 
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        for d in drawable:
+            d.draw(screen)
+
+        pygame.display.flip()
+        print(dt)
 
 
 if __name__ == "__main__":
